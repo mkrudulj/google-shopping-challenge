1e69d5fb2c7eeeb0ccc0636458d4c2c946ec9502
mkrudulj <mkrudulj@gmail.com>
2014-11-10 20:06:14 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'GoogleShopping.php';
+
+$ean = '8806085553941';
+
+$crawler = new GoogleShopping;
+$prices = $crawler->getPrices($ean);
+
+echo "<PRE>", json_encode($prices, JSON_PRETTY_PRINT), "</PRE>";
+
+?>
